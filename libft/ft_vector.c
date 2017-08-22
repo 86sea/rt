2b445ca21389ac3309317d4dad9efadb13f9f79a
Sean YOUNG <syoung@e4r5p5.wtc.co.za>
2017-08-22 12:17:02 +0200

    norm

--- libft/ft_vector.c
+++ libft/ft_vector.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_vector.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syoung <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/22 12:14:39 by syoung            #+#    #+#             */
+/*   Updated: 2017/08/22 12:14:42 by syoung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -9,14 +20,13 @@ t_vec3f		ft_add_vectors(t_vec3f v0, t_vec3f v1)
 	i = -1;
 	while (++i < 3)
 		v.vec[i] = v0.vec[i] + v1.vec[i];
-
 	return (v);
 }
 
 t_vec3f		ft_subtract_vectors(t_vec3f v0, t_vec3f v1)
 {
-	int 	i;
-	t_vec3f v;
+	int		i;
+	t_vec3f	v;
 
 	i = -1;
 	while (++i < 3)
@@ -24,14 +34,13 @@ t_vec3f		ft_subtract_vectors(t_vec3f v0, t_vec3f v1)
 	return (v);
 }
 
-t_vec3f			ft_multi_vector(t_vec3f v, float t)
+t_vec3f		ft_multi_vector(t_vec3f v, float t)
 {
 	int		i;
 
 	i = -1;
 	while (++i < 3)
 		v.vec[i] *= t;
-
 	return (v);
 }
 
@@ -44,7 +53,7 @@ float		ft_magnitude(t_vec3f v)
 	i = -1;
 	while (++i < 3)
 		t += v.vec[i] * v.vec[i];
-	return(sqrt(t));
+	return (sqrt(t));
 }
 
 float		ft_dot_prod(t_vec3f v0, t_vec3f v1)
